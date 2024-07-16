84802292d99620084f3e498df68fe1cf8924a9df
Steven Gong <gonghaima@hotmail.com>
2024-07-16 17:22:24 +1000

    skip operator

--- linq-example-2/Program.cs
+++ linq-example-2/Program.cs
@@ -161,18 +161,25 @@ class Program
         // }
 
         // Union
-        var employeesList2 = new List<Employee> {
-            new Employee { Id = 1, FirstName = "Bob", LastName = "Jones", AnnualSalary = 60000.3m, IsManager = true, DepartmentId = 1 },
-            new Employee { Id = 2, FirstName = "Sarah", LastName = "Jameson", AnnualSalary = 80000.1m, IsManager = true, DepartmentId = 3 },
-            new Employee { Id = 3, FirstName = "Douglas", LastName = "Roberts", AnnualSalary = 40000.2m, IsManager = false, DepartmentId = 1 },
-            new Employee { Id = 9, FirstName = "Jane", LastName = "Stevens", AnnualSalary = 30000.2m, IsManager = false, DepartmentId = 3 }
-        };
-        var result = employeesList.Union(employeesList2, new EmployeeComparer());
+        // var employeesList2 = new List<Employee> {
+        //     new Employee { Id = 1, FirstName = "Bob", LastName = "Jones", AnnualSalary = 60000.3m, IsManager = true, DepartmentId = 1 },
+        //     new Employee { Id = 2, FirstName = "Sarah", LastName = "Jameson", AnnualSalary = 80000.1m, IsManager = true, DepartmentId = 3 },
+        //     new Employee { Id = 3, FirstName = "Douglas", LastName = "Roberts", AnnualSalary = 40000.2m, IsManager = false, DepartmentId = 1 },
+        //     new Employee { Id = 9, FirstName = "Jane", LastName = "Stevens", AnnualSalary = 30000.2m, IsManager = false, DepartmentId = 3 }
+        // };
+        // var result = employeesList.Union(employeesList2, new EmployeeComparer());
+        // foreach (var item in result)
+        // {
+        //     Console.WriteLine($"{item.Id} {item.FirstName} {item.LastName}");
+        // }
+
+        // Partitioning Operator - Skip, SkipWhile, Take, TakeWhile
+        // Skip
+        var result = employeesList.Skip(2);
         foreach (var item in result)
         {
             Console.WriteLine($"{item.Id} {item.FirstName} {item.LastName}");
         }
-
 
 
 
